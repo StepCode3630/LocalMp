665511320921493002a892343fb76cf6c943f799
Stepan <stepanpatricny@proton.me>
2026-06-03 15:44:14 +0200

    feat: header
    [wip] [45]

---
+++ front-end/src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <div class="header">
+    <img src="../assets/music-library-svgrepo-com 2.svg" alt="logo" />
+    <div>
+      <button>Log in</button>
+      <img src="../assets/moon.svg" alt="" />
+    </div>
+  </div>
+</template>
